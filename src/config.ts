db505ab808ad2fd6c3f9905de228ea104d6d9b5c
Mauricio Robayo <rfmajo@gmail.com>
2021-05-19 10:13:47 -0500

    fix: title divider
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const channelsMetaInfo: Record<
   string,
-  { name: string; titleDivider: "|" | "-" }
+  { name: string; titleDivider: " | " | " - " }
 > = {
-  UCAuUUnT6oDeKwE6v1NGQxug: { name: "TEDtalksDirector", titleDivider: "|" },
-  "UCsT0YIqwnpJCM-mx7-gSA4Q": { name: "TEDxTalks", titleDivider: "|" },
-  UCsooa4yRKGN_zEE8iknghZA: { name: "TEDEducation", titleDivider: "-" },
+  UCAuUUnT6oDeKwE6v1NGQxug: { name: "TEDtalksDirector", titleDivider: " | " },
+  "UCsT0YIqwnpJCM-mx7-gSA4Q": { name: "TEDxTalks", titleDivider: " | " },
+  UCsooa4yRKGN_zEE8iknghZA: { name: "TEDEducation", titleDivider: " - " },
 };
 
 export const channelsIds = Object.keys(channelsMetaInfo);
