4ef1733012b6f62eeed0128543469b960a615e63
Mauricio Robayo <rfmajo@gmail.com>
2021-05-21 18:28:56 -0500

    chore: update channels names
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ export const channelsMetaInfo: Record<
   string,
   { name: string; titleDivider: " | " | " - " }
 > = {
-  UCAuUUnT6oDeKwE6v1NGQxug: { name: "TEDtalksDirector", titleDivider: " | " },
-  "UCsT0YIqwnpJCM-mx7-gSA4Q": { name: "TEDxTalks", titleDivider: " | " },
-  UCsooa4yRKGN_zEE8iknghZA: { name: "TEDEducation", titleDivider: " - " },
+  UCAuUUnT6oDeKwE6v1NGQxug: { name: "TED", titleDivider: " | " },
+  "UCsT0YIqwnpJCM-mx7-gSA4Q": { name: "TEDx Talks", titleDivider: " | " },
+  UCsooa4yRKGN_zEE8iknghZA: { name: "TED-Ed", titleDivider: " - " },
 };
 
 export const channelsIds = Object.keys(channelsMetaInfo);
